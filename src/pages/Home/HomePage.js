9e448637668e79b8d205435d56097949eab1cde2
Jacob Christensen <jchristensen38@gatech.edu>
2023-08-08 12:34:24 -0500

    updated more css on home page and app css

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,7 +1,14 @@
-import { ProfileImage } from "../../components/ProfileImage"
+import { ProfileImage } from "../../components/ProfileImage";
+import "./homepage.css";
 
 export const HomePage = () => {
     return (
-        <ProfileImage></ProfileImage>
+        <div class="grid-container">
+            <div class="grid-item">
+                <ProfileImage/>
+            </div>
+            <div class="grid-item">Item 2</div>
+            <div class="grid-item">Item 3</div>
+        </div>
     );
 }
