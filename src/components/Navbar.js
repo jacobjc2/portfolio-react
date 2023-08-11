cd7c1737269afa57f786e52d0b06fe3c16125c68
Jacob Christensen <jchristensen38@gatech.edu>
2023-08-11 09:20:23 -0500

    updated nav and home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from "react-router-dom";
+import logo_white from '../assets/logo_white.png'
 
 
 export const Navbar = () => {
@@ -31,27 +32,29 @@ export const Navbar = () => {
     ]
 
     return (
-        <div className="flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed">
+        <div className="flex justify-between items-center w-full h-20 text-white bg-indigo-950 px-4 fixed">
             <div>
-                <h1 className="text-5xl ml-2">My Portfolio</h1>
+                <a href="/">
+                <img className="object-scale-down h-20 px-4" src={logo_white} alt="logo"/>
+                </a>
             </div>
             <ul className="hidden md:flex">
                 {links.map(({id, link, href}) => (
                     <li key={id} className="px-4 cursor-pointer capitalize font-medium
-                    text-gray-400 hover:scale-105 duration-200"><Link to={href}>{link}</Link></li>
+                    text-gray-200 hover:scale-105 duration-200"><Link to={href}>{link}</Link></li>
                 ))}
             </ul>
             
-            <div onClick={() => setNav(!nav)}  className="cursor-pointer pr-4 z-10 text-gray-400 md:hidden">
+            <div onClick={() => setNav(!nav)}  className="cursor-pointer pr-4 z-10 text-gray-200 md:hidden">
                 {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
             </div>
 
             {nav && (
                 <ul className="flex flex-col justify-center items-center absolute top-0 left-0 
-                w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-400">
+                w-full h-screen bg-gradient-to-b from-indigo-950 to-violet-800 text-gray-200 bg-grad">
                     {links.map(({id, link, href}) => (
                         <li key={id} className="px-4 cursor-pointer capitalize text-4xl py-6">
-                            <a clas href={href}></a>{link}
+                            <Link to={href}>{link}</Link>
                         </li>
                     ))}
                 </ul>
