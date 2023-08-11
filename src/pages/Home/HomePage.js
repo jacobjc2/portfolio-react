04d02cfa158deaed22828a648900c56fe1dc7444
Jacob Christensen <jchristensen38@gatech.edu>
2023-08-10 20:33:29 -0500

    Commiting homepage changes

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -6,9 +6,15 @@ export const HomePage = () => {
         <div class="grid-container">
             <div class="grid-item">
                 <ProfileImage/>
+                <p class="name">Jacob Christensen</p>
             </div>
-            <div class="grid-item">Item 2</div>
-            <div class="grid-item">Item 3</div>
+            <div class="grid-item about">
+                <p class="title">About Me</p>
+                <p class="about-content">
+                    Hello
+                </p>
+            </div>
+            {/* <div class="grid-item name">Jacob Christensen</div> */}
         </div>
     );
 }
